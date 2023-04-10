11d0fb10cd177125dcacde2f423d25130febb52a
Rishabh tiwari <105351344+rishabhtiwari2023@users.noreply.github.com>
2023-04-10 08:14:26 +0530

    Update server.js

--- RenderTest/server.js
+++ RenderTest/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Successful response.');
-});
+ app.get("/", function (request, response){
+     //show this file when the "/" is requested
+     response.sendFile(__dirname+"/views/index.html");
+ });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
